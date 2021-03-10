5708aca7232a1b5fc545b69029f63da9acb04dcc
Gunpowderman <adel.baroody@gmail.com>
2021-03-10 18:59:03 +0300

    express task 3

--- src/app.js
+++ src/app.js
@@ -1,13 +1,27 @@
+//dependancies
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
+const slugify = require("slugify");
 
+//exports
 let data = require("./data");
 
 const app = express();
+
 app.use(cors());
+app.use(bodyParser.json());
 
 app.get("/products", (_, response) => {
   response.json(data);
+});
+
+app.post("/products", (request, response) => {
+  const id = data[data.length - 1].id + 1;
+  const slug = slugify(request.body.name, { lower: true });
+  const newProduct = { id, slug, ...request.body };
+  data.push(newProduct);
+  response.status(201).json(newProduct);
 });
 
 app.delete("/products/:productId", (request, response) => {
