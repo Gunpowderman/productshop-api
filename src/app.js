ca448a8ab15a25ab0edba80b85f792b1c9e87aa2
Gunpowderman <adel.baroody@gmail.com>
2021-03-10 19:17:48 +0300

    task 8 routes

--- src/app.js
+++ src/app.js
@@ -2,38 +2,15 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const slugify = require("slugify");
 
-//exports
-let data = require("./data");
+//imports
+const productsRoutes = require("./routes/products");
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
-
-app.get("/products", (_, response) => {
-  response.json(data);
-});
-
-app.post("/products", (request, response) => {
-  const id = data[data.length - 1].id + 1;
-  const slug = slugify(request.body.name, { lower: true });
-  const newProduct = { id, slug, ...request.body };
-  data.push(newProduct);
-  response.status(201).json(newProduct);
-});
-
-app.delete("/products/:productId", (request, response) => {
-  const { productId } = request.params;
-  const foundProduct = data.find((product) => product.id === +productId);
-  if (foundProduct) {
-    data = data.filter((product) => product !== foundProduct);
-    response.status(204).end();
-  } else {
-    response.status(404).json({ message: "Product not found" });
-  }
-});
+app.use("/products", productsRoutes);
 
 app.listen(8000, () => {
   console.log("server is up");
